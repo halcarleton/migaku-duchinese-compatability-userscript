dceeae652962b794d113129c826a8f158fb43389
Hal Carleton <hello@halleycarleton.com>
2024-10-29 14:25:47 -0700

    chore: configure svelte to output custom elements

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { build } from 'vite'
 
 export default {
+  compilerOptions: {
+    customElement: true,
+  },
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
