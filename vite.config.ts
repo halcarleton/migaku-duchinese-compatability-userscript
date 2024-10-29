7078f88b46bcd6ae86adfb7f4903fe544ed85848
Hal Carleton <hello@halleycarleton.com>
2024-10-29 14:23:22 -0700

    chore: configure build to output userscript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import monkey from "vite-plugin-monkey";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  build: {
+    minify: false,
+  },
+  plugins: [
+    svelte(),
+    monkey({
+      entry: "src/main.ts",
+      userscript: {
+        author: "https://github.com/halcarleton",
+        description:
+          "Modifies the DuChinese graded reader UI to make it compatible with the Migaku extension.",
+        license: "Apache 2.0",
+        namespace: "https://github.com/halcarleton",
+        include: ["https://duchinese.net/lessons/*"],
+      },
+    }),
+  ],
+});
