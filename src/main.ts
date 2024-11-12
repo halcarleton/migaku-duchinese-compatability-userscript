a7a70fb0a586d0c1703b691eaec4ced627fb47a6
Hal Carleton <hello@halleycarleton.com>
2024-11-11 16:10:16 -0800

    feat: integrate plain text UI using navigate events

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,40 @@
+import initialize from "./lib/initialize";
+import { isLessonPage } from "./lib/url";
+
+interface NavigateEvent extends Event {
+  destination: { url: string };
+}
+
 async function main() {
-  console.log("Test");
+  let currentUrl = "";
+  const { mount, mounted, refresh, unmount } = await initialize();
+
+  async function handleNavigate(url: string): Promise<void> {
+    const isDupeNavigation = currentUrl === url;
+    const toLessonPage = isLessonPage(url);
+
+    if (isDupeNavigation) {
+      return;
+    }
+
+    currentUrl = url;
+
+    if (mounted() && toLessonPage) {
+      await refresh();
+    } else if (mounted() && !toLessonPage) {
+      await unmount();
+    } else if (!mounted() && toLessonPage) {
+      await mount();
+    }
+  }
+
+  function handleNavigateEvent(e: NavigateEvent) {
+    return handleNavigate(e.destination.url);
+  }
+
+  window.addEventListener("popstate", handleNavigateEvent as any);
+
+  navigation.addEventListener("navigate", handleNavigateEvent);
 }
 
 main();
